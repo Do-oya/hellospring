073eaf11d838a57a399193c0a1c6c3db07bd46d2
Do-oya <qkrwjdgus1441@naver.com>
2024-08-20 22:14:24 +0900

    24/08/20

---
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -0,0 +1,20 @@
+package tobyspring.hellospring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.payment.Payment;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
+public class Client {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
+        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment1: " + payment1);
+    }
+}
---
+++ src/main/java/tobyspring/hellospring/payment/Payment.java
@@ -0,0 +1,58 @@
+package tobyspring.hellospring.payment;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount; // double 오차가 생길수 있음 돈, 계산 관련 자바제공 BigDecimal 사용 바람직
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+}
