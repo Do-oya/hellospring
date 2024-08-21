1271359220a7e7b0e5ed2dba279823e6fab85176
Do-oya <qkrwjdgus1441@naver.com>
2024-08-21 15:22:05 +0900

    24/08/21 (Clock을 이용한 시간 테스트)

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -1,28 +1,49 @@
 package tobyspring.hellospring.payment;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.*;
 
 public class PaymentServiceTest {
+    Clock clock;
 
-    @Test
-    void prepare() throws IOException {
-        testAmount(valueOf(500), valueOf(5_000));
-        testAmount(valueOf(1_000), valueOf(10_000));
-        testAmount(valueOf(3_000), valueOf(30_000));
-
-        //원화환산금액의 유효시간 계산
-//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    @BeforeEach
+    void beforeEach() {
+        this.clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
     }
 
-    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
-        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+    @Test
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000), this.clock);
+        testAmount(valueOf(1_000), valueOf(10_000), this.clock);
+        testAmount(valueOf(3_000), valueOf(30_000), this.clock);
+    }
+
+    @Test
+    void validUntil() throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(1_000)), clock);
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // vaild until이 prepare() 30분 뒤로 설정됐는가?
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount, Clock clock) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate), clock);
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
