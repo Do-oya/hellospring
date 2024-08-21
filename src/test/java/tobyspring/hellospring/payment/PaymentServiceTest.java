80ba7f43f97a75e7f85ae8d80f882dae733f78ff
Park JeongHyun <qkrwjdgus1441@naver.com>
2024-08-21 13:36:48 +0900

    24/08/21 (테스트와 DI(1))

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -0,0 +1,33 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.Assertions.*;
+
+public class PaymentServiceTest {
+
+    @Test
+    void prepare() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+
+        //원화환산금액의 유효시간 계산
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
+    }
+}
