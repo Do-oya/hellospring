6499842b76835f1b92a26b8bef565ceb3451654a
Do-oya <qkrwjdgus1441@naver.com>
2024-08-22 20:56:36 +0900

    24/08/22 (OrderRepository, Exception)

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -1,9 +1,9 @@
 package tobyspring.hellospring;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -11,17 +11,19 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
 
         Order order = new Order("100", BigDecimal.TEN);
-        em.persist(order);
+        repository.save(order);
 
         System.out.println(order);
 
-        em.getTransaction().commit();
-        em.close();
+        try{
+            Order order2 = new Order("100", BigDecimal.ONE);
+            repository.save(order2);
+        }
+        catch (ConstraintViolationException e){
+            System.out.println("주문번호 충돌을 복구하는 작업");
+        }
     }
 }
