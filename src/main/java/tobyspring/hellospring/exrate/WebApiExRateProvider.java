90760ddb2a65a44551a7c0a41146b8cb3f7c1a9e
Do-oya <qkrwjdgus1441@naver.com>
2024-08-22 14:47:09 +0900

    24/08/22 (디폴트 콜백과 템플릿 빈)

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -7,11 +7,16 @@ import java.math.BigDecimal;
 
 
 public class WebApiExRateProvider implements ExRateProvider {
-    ApiTemplate apiTemplate = new ApiTemplate();
+    private final ApiTemplate apiTemplate;
+
+    public WebApiExRateProvider(ApiTemplate apiTemplate) {
+        this.apiTemplate = apiTemplate;
+    }
+
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return apiTemplate.getExRate(url, new HttpClientApiExercutor(), new ErApiExRateExtractor());
+        return apiTemplate.getForExRate(url);
     }
 }
